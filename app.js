bff0a6259a12adc7b7c6204a4f125f9f345246df
Roannel Fernández Hernández <roannel.fdez@gmail.com>
2021-03-18 00:33:35 -0400

    graphql support

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const { graphqlHTTP } = require("express-graphql");
+const graphql = require("graphql");
+
+const QueryRoot = new graphql.GraphQLObjectType({
+  name: "Query",
+  fields: () => ({
+    hello: {
+      type: graphql.GraphQLString,
+      resolve: () => "Hello world!"
+    }
+  })
+});
+
+const schema = new graphql.GraphQLSchema({ query: QueryRoot });
 
 var app = express();
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use(
+  "/api",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true
+  })
+);
 
-var listener = app.listen(8080, function() {
+var listener = app.listen(4000, function () {
   console.log("Listening on port " + listener.address().port);
 });
